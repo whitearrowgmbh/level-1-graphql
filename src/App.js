c9e60702b7d322f06b9a222284ff93f8969ae7a8
juerg <shop@whitearrow.de>
2018-08-06 13:01:33 +0200

    Kap. 21 Ende, alles geht (cache persist)

--- src/App.js
+++ src/App.js
@@ -1,27 +1,11 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ApolloClient from 'apollo-boost';
-import {ApolloProvider} from 'react-apollo';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import InsertPost from './posts/InsertPost';
 import Post from './posts/Post';
 import Posts from './posts/Posts';
 
-
-const defaultState = {
-	isEditMode: false
-};
-
-const client = new ApolloClient(
-	{
-		uri        : 'https://api-euwest.graphcms.com/v1/cjkcqrlqq2dmv01gmyk7r9hhn/master',
-		clientState: {
-			defaults : defaultState,
-			resolvers: {}
-		}
-	}
-);
 
 
 //client.query({
@@ -32,7 +16,6 @@ class App extends Component {
 	render()
 	{
 		return (
-			<ApolloProvider client={client}>
 				<Router>
 					<div className="App">
 						<header className="App-header">
@@ -59,7 +42,6 @@ class App extends Component {
 						</main>
 					</div>
 				</Router>
-			</ApolloProvider>
 		);
 	}
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,38 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {InMemoryCache} from 'apollo-cache-inmemory';
+import {persistCache} from 'apollo-cache-persist';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from 'react-apollo';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+const defaultState = {
+	isEditMode: false
+};
+
+
+const cache = new InMemoryCache();
+
+persistCache({
+				 cache,
+				 storage: window.localStorage
+			 })
+	.then(() => {
+		const client = new ApolloClient(
+			{
+				cache,
+				uri        : 'https://api-euwest.graphcms.com/v1/cjkcqrlqq2dmv01gmyk7r9hhn/master',
+				clientState: {
+					defaults : defaultState,
+					resolvers: {}
+				}
+			}
+		);
+
+		ReactDOM.render(
+			<ApolloProvider client={client}>
+				<App/>
+			</ApolloProvider>, document.getElementById('root'));
+		registerServiceWorker();
+	});
